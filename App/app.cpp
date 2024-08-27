90b8b2565b1168b554f5e977bae03ad43980c9d6
Phanit-lab <keosophanit@outlook.com>
2024-08-27 10:55:06 +0700

    add Function addWord

--- App/app.cpp
+++ App/app.cpp
@@ -6,29 +6,55 @@ using namespace std;
 bool login(string username, string password);
 void controlMenu(int control);
 void adminMenu(int choise);
-void registerUser();
+void registerUser(string &name, string &username, string &password, int &id, int &role);
+void addword(string word[], int &currentcout);
+void displayWord(string word[], int &currentcout);
+// Enum:
 
 // Main Function
-
+const int MAX_WORD = 10;
+string word[MAX_WORD];
+int currentcout = 0;
 int main()
 {
+
     int loginChoice;
+    string name, username, password;
+    int id, role;
 
     do
     {
+        enum Login_Menu
+        {
+            EXIT,
+            REGISTER,
+            LOGIN
+        };
         displayMenuLogin();
         cin >> loginChoice;
         switch (loginChoice)
         {
-            enum
-            {
-                EXIT,
-                REGISTER,
-                LOGIN
-            };
+
         case REGISTER:
-            registerUser();
+            registerUser(name, username, password, id, role);
             break;
+        case 6:
+            cout << "Name: " << name << endl;
+            cout << "username: " << username << endl;
+            cout << "password: " << password << endl;
+            cout << "in: " << id << endl;
+            if (role == 1)
+            {
+                cout << "Role: Admin" << endl;
+            }
+            else if (role == 2)
+            {
+                cout << "Role: Player" << endl;
+            }
+            else
+            {
+                cout << RED << "Role not found!!" << RESET << endl;
+            }
         case LOGIN:
         {
             string username, password;
@@ -84,78 +110,82 @@ bool login(string username, string password)
         return false;
     }
 }
-
 void controlMenu(int control)
 {
-    switch (control)
     {
-        enum Menu
-        {
-            EXIT,
-            ADD,
-            EDIT,
-            DELETE,
-            SEARCH,
-            SHORT,
-            DISPLAY,
-        };
-    case ADD:
-    {
-        cout << GREEN << "ADD... Successful!!" << RESET << endl;
-        break;
-    }
-    case EDIT:
-    {
-        cout << GREEN << "EDIT... Successful!!" << RESET << endl;
-        break;
-    }
-    case DELETE:
-    {
-        cout << GREEN << "DELETE... Successful!!" << RESET << endl;
-        break;
-    }
-    case SEARCH:
-    {
-        cout << GREEN << "SEARCH... Successful!!" << RESET << endl;
-        break;
-    }
-    case SHORT:
-    {
-        cout << GREEN << "SHORT... Successful!!" << RESET << endl;
-        break;
-    }
-    case DISPLAY:
-    {
-        cout << GREEN << "DISPLAY... Successful!!" << RESET << endl;
-        break;
-    }
-    case EXIT:
-    {
-        cout << YELLOW << "Back to the USER Menu..." << RESET << endl;
-        break;
-    }
 
-    default:
-    {
-        cout<<RED<<"Invalid choise"<<RESET<<endl;
-        break;
-    }
+        switch (control)
+        {
+            enum Menu
+            {
+                EXIT,
+                ADD,
+                EDIT,
+                DELETE,
+                SEARCH,
+                SHORT,
+                DISPLAY,
+            };
+        case ADD:
+        {
+            addword(word, currentcout);
+            cout << GREEN << "ADD... Successful!!" << RESET << endl;
+            break;
+        }
+        case EDIT:
+        {
+            cout << GREEN << "EDIT... Successful!!" << RESET << endl;
+            break;
+        }
+        case DELETE:
+        {
+            cout << GREEN << "DELETE... Successful!!" << RESET << endl;
+            break;
+        }
+        case SEARCH:
+        {
+            cout << GREEN << "SEARCH... Successful!!" << RESET << endl;
+            break;
+        }
+        case SHORT:
+        {
+            cout << GREEN << "SHORT... Successful!!" << RESET << endl;
+            break;
+        }
+        case DISPLAY:
+        {
+            displayWord(word, currentcout);
+            cout << GREEN << "DISPLAY... Successful!!" << RESET << endl;
+            break;
+        }
+        case EXIT:
+        {
+            cout << YELLOW << "Back to the USER Menu..." << RESET << endl;
+            break;
+        }
+
+        default:
+        {
+            cout << RED << "Invalid choise" << RESET << endl;
+            break;
+        }
+        }
     }
 }
 void adminMenu(int choise)
+
 {
+    enum Menu
+    {
+        EXIT,
+        WORD,
+        ACCOUNT,
+        PLAY
+    };
     switch (choise)
     {
         {
             int control;
-
-            enum Menu
-            {
-                EXIT,
-                WORD,
-                ACCOUNT,
-                PLAY
-            };
         case WORD:
         {
             int controlWord;
@@ -196,9 +226,29 @@ void adminMenu(int choise)
         }
     }
 }
-void registerUser()
+void addword(string word[], int &currentcout)
 {
-    string name, username, password, id, role;
+    cout << BRIGHT_BLUE << "---------ADD_WORD--------" << RESET << endl;
+    int count;
+    cout << "How many words you want to add? : ";
+    cin >> count;
+    for (size_t i = 0; i < count; i++)
+    {
+        cout << "Word [" << currentcout + 1 << "] :";
+        cin >> word[currentcout];
+        currentcout++;
+    }
+}
+void displayWord(string word[], int &currentcout)
+{
+    cout << BRIGHT_BLUE << "--------LIST_WORDS-------" << RESET << endl;
+    for (int i = 0; i <= currentcout; i++)
+    {
+        cout << "Word [" << i + 1 << "] : " << word[i] << endl;
+    }
+}
+void registerUser(string &name, string &username, string &password, int &id, int &role)
+{
     cout << BRIGHT_BLUE << "---------REGISTER--------" << RESET << endl;
     cout << " Name: ";
     cin >> name;
